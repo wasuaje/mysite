07f85ef966524483b21faf41efc38c74e2fc1837
Wuelfhis Asuaje <wuelfhis.asuaje@jpmorgan.com>
2018-04-16 13:49:52 -0300

    adding django docker tests

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from app.models import Signup
+
+
+class SignupTestCase(TestCase):
+    def setUp(self):
+        Signup.objects.create(email="wasuaje@gmail.com")
+        Signup.objects.create(email="wasuaje@hotmail.com")
+
+    def test_signup_are_correct(self):
+        """Animals that can speak are correctly identified"""
+        gm = Signup.objects.get(name="wasuaje@gmail.com")
+        hm = Signup.objects.get(name="wasuaje@hotmail.com")
+        self.assertEqual(gm.email, 'wasuaje@gmail.com')
+        self.assertEqual(hm.email, 'wasuaje@hotmail.com')
